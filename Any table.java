761462ede51c2e4192687d1908ebe2a8f7b18755
Sai Rama Krishna <sairamakri5924@gmail.com>
2026-07-22 23:55:03 +0530

    Change main method to static in Main class

--- Any table.java
+++ Any table.java
@@ -1,6 +1,6 @@
 import java.util.*
 class Main{
-  public void main (String [] args){
+  public static void main (String [] args){
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     for(int i=0; i<=10; i++){
